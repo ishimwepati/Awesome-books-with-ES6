4d80c3514ed9e3fbe2bb345d539c62a8ba852338
ishimwepati <wazacode@gmail.com>
2023-07-18 23:08:22 +0300

    Remove node

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // index.js
-import listnav from './modules/nav.js';
-import ManageBookDetails from './modules/manageBookDetails.js';
+import listnav from './modules/nav.js';                                                                               // eslint-disable-line max-classes-per-file
+import ManageBookDetails from './modules/manageBookDetails.js';                                                        // eslint-disable-line max-classes-per-file
 import getCurrentDateTime from './modules/data_time.js';
 
 const now = getCurrentDateTime();
-// Convert the time to East African Time (EAT)
+// Convert the time to my East African Time (EAT)
 const eastAfricanTime = now.setZone('Africa/Nairobi');
 // Format the time as a string
 const formattedTime = eastAfricanTime.toFormat('yyyy-MM-dd HH:mm:ss');
