9d50bb88a08d0c2b10028aee9368ae893c4dcb7c
ishimwepati <wazacode@gmail.com>
2023-07-18 22:23:17 +0300

    Files added

---
+++ index.js
@@ -0,0 +1,14 @@
+// index.js
+import * as nav from './modules/nav.js';
+import * as ManageBookDetails from './modules/manageBookDetails.js';
+import { getCurrentDateTime } from './modules/data_time.js';
+
+        const now = getCurrentDateTime();
+        // Convert the time to East African Time (EAT)
+        const eastAfricanTime = now.setZone('Africa/Nairobi');
+        // Format the time as a string
+        const formattedTime = eastAfricanTime.toFormat('yyyy-MM-dd HH:mm:ss');
+        // Display the time in East African Time
+        const outputElement = document.getElementById('luxondate');
+        outputElement.textContent = `Current Time in East African Time (EAT): ${formattedTime}`;
+    
